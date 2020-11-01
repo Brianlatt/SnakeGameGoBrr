316221ee36d7c98659680ef4fee350244947fd6d
Brianlatt <brianlatt1@gmail.com>
2020-11-01 17:37:58 +0000

    Menu
    Started A Menu And Added Play, Settings and Credits with exit button, Right now Only play works.

---
+++ Assets/Scripts/ManageButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManageButton : MonoBehaviour
+{
+    // Loads the scene listed From play button
+    public void PlayButton(string newGame)
+    {
+        SceneManager.LoadScene(newGame);
+        Time.timeScale = 1f;
+    }
+
+    // Exit Button
+    public void ExitButton()
+    {
+        Application.Quit();
+        Debug.Log("Game Closed");
+    }
+}
